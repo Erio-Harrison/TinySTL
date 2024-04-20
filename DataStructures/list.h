9f94e1947ffce83fb451514753d5a70390e79059
Harrison Erio <u7541840@gmail.com>
2024-04-20 21:00:59 +0800

    update list

--- DataStructures/list.h
+++ DataStructures/list.h
@@ -4,6 +4,7 @@
 #include "../Allocators/construct.h"
 
 namespace TinySTL{
+
     //Node
     template <class T>
     struct __list_node{
@@ -182,9 +183,88 @@ public:
     {
         if(position != last)
         {
-            /*TODO*/
+            (*(link_type((*last.node).prev))).next = position.node;
+            (*(link_type((*first.node).prev))).next = last.node;
+            (*(link_type((*position.node).prev))).next = first.node;
+            link_type tmp = link_type((*position.node).prev);
+            (*last.node).prev = (*first.node).prev;
+            (*first.node).prev = tmp;
         }
     }
 
+    //针对不同接口，splice有不同版本
+    void splice(iterator position, list& x)
+    {
+        if(x.empty())
+            transfer(position,x.begin(),x.end());
+    }
+
+    void splice(iterator position, list&, iterator i)
+    {
+        iterator j =i;
+        ++j;
+        if(position == i || position == j) return;
+        transfer(position,i,j);
+    }
+
+    void splice(iterator position, list&, iterator first, iterator last)
+    {
+        if(first != last)
+            transfer(position,first,last);
+    }
+
+    void merge(list<T, Alloc>& x)
+    {
+        iterator first1 = begin();
+        iterator last1 = end();
+        iterator first2 = x.begin();
+        iterator last2 = x.end();
+
+        while(first1 != last1 && first2 != last2)
+            if(*first2 < *first1){
+                iterator next = first2;
+                transfer(first1,first2,++next);
+                first2 = next;
+            }
+            else
+                ++first1;
+        if(first2 != last2) transfer(last1,first2,last2);
+    }
+
+    void reverse(){
+        if(node->next == node|| link_type(node->next)->next == node) return;
+        iterator first = begin();
+        ++first;
+        while(first != end()){
+            iterator old = first;
+            ++first;
+            transfer(begin(),old,first);
+        }
+    }
+
+    //list不能用STL的sort()，因为STL的sort()只接受RamdonAccessIterator
+    void sort(){
+        if(node -> next == node || link_type(node -> next)->next == node) return;
+        list<T,Alloc> carry;
+        list<T,Alloc> counter[64];
+        int fill = 0;
+        while(!empty())
+        {
+            carry.splice(carry.begin(),*this,begin());
+            int i = 0;
+            while(i < fill && !counter[i].empty())
+            {
+                counter[i].merge(carry);
+                carry.std::swap(counter[i++]);
+            }
+            carry.swap(counter[i]);
+            if(i == fill) ++fill;
+        }
+        for(int i = 1; i< fill; ++i)
+            counter[i].merge(counter[i-1]);
+        std::swap(counter[fill-1]);
+    }
 };
+
+
 
